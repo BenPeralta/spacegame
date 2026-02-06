b07cd7d7a50fee263e2669041d9d8bc391c28176
BenPeralta <benperalta24@gmail.com>
2026-02-06 14:40:41 -0600

    Initial Commit

---
+++ Stellar Ascent/Stellar_AscentApp.swift
@@ -0,0 +1,17 @@
+//
+//  Stellar_AscentApp.swift
+//  Stellar Ascent
+//
+//  Created by Benjamin Peralta on 2/6/26.
+//
+
+import SwiftUI
+
+@main
+struct Stellar_AscentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
